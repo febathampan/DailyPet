12a50c9d5950b62281157fc35692c398f3e56f52
febathedeveloper <febathedeveloper@gmail.com>
2024-09-23 11:01:18 -0400

    Initial setup commit

---
+++ src/main/java/com/technoscribers/dailypet/DailypetApplication.java
@@ -0,0 +1,13 @@
+package com.technoscribers.dailypet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailypetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailypetApplication.class, args);
+	}
+
+}
