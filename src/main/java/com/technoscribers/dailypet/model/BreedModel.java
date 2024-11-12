5db0affebb1917508eccfe8252a6262db9df58a6
febathedeveloper <febathedeveloper@gmail.com>
2024-11-12 18:15:33 -0500

    Added breed details to name field

--- src/main/java/com/technoscribers/dailypet/model/BreedModel.java
+++ src/main/java/com/technoscribers/dailypet/model/BreedModel.java
@@ -9,7 +9,7 @@ public class BreedModel {
     private Long id;
     private Integer lifeSpan;
     private String temperament;
-    private String breedDescription;
+    private String name;
     private String typeOfPet;
     private String category;
     private String petTypeDescription;
